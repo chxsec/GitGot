abb774299de7e783dcd78d474173782f3bc8cd71
chxsec <98628139+chxsec@users.noreply.github.com>
2026-04-22 14:30:23 -0700

    Update GitHub authentication method in gitgot.py
    fix
    DeprecationWarning: Argument login_or_token is deprecated, please use auth=github.Auth.Token(...) instead   g = github.Github(ACCESS_TOKEN)

--- gitgot.py
+++ gitgot.py
@@ -513,9 +513,9 @@ def main():
 
     if args.url:
         g = github.Github(base_url=args.url + "/api/v3",
-                          login_or_token=ACCESS_TOKEN)
+                          auth=github.Auth.Token(ACCESS_TOKEN))
     else:
-        g = github.Github(ACCESS_TOKEN)
+        g = github.Github(auth=github.Auth.Token(ACCESS_TOKEN))
 
 
     if state.is_gist:
